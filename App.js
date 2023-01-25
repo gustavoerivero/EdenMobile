0cc728da49692bf32ce4137d4128f71c7001ed44
Gustavo Rivero <gustavoerivero12@gmail.com>
2023-01-25 06:50:15 -0400

    Update

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from 'react'
+import SplashScreen from 'react-native-splash-screen'
 import { NavigationContainer } from '@react-navigation/native'
 import { NativeBaseProvider } from 'native-base'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
@@ -5,17 +7,22 @@ import { AuthProvider } from './src/context/AuthContext'
 import StackNavigation from './src/navigation/StackNavigation'
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <SafeAreaProvider>
-        <NavigationContainer>
-          <NativeBaseProvider>
-            <AuthProvider>
-              <SafeAreaView style={{ flex: 1 }}>
-                <StackNavigation />
-              </SafeAreaView>
-            </AuthProvider>
-          </NativeBaseProvider>
-        </NavigationContainer>
+      <NavigationContainer>
+        <NativeBaseProvider>
+          <AuthProvider>
+            <SafeAreaView style={{ flex: 1 }}>
+              <StackNavigation />
+            </SafeAreaView>
+          </AuthProvider>
+        </NativeBaseProvider>
+      </NavigationContainer>
     </SafeAreaProvider>
   )
 }
